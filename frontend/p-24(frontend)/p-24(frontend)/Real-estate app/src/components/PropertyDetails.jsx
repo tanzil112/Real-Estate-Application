3661d83cdbe076ea06fa3a918947acb573f9c17e
4749swathisaimpu@gmail.com <4749swathisaimpu@gmail.com>
2025-02-26 16:38:43 +0530

    updated

--- frontend/p-24(frontend)/p-24(frontend)/Real-estate app/src/components/PropertyDetails.jsx
+++ frontend/p-24(frontend)/p-24(frontend)/Real-estate app/src/components/PropertyDetails.jsx
@@ -100,11 +100,13 @@ const PropertyDetails = () => {
         {/* Location Section */}
         <div className="location-section">
           <h2>Location</h2>
-          <iframe
-            width="800"
-            height="600"
-            src="https://www.openstreetmap.org/export/embed.html?bbox=77.5946,12.9716,77.5946,12.9716&layer=mapnik"
-          ></iframe>
+          <iframe 
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7611.461467942758!2d78.4747512!3d17.385044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb99daeaebd2c7%3A0xae93b78392bafbc2!2sHyderabad%2C%20Telangana!5e0!3m2!1sen!2sin!4v1708424581234!5m2!1sen!2sin" 
+    width="600" 
+    height="450" 
+   >
+</iframe>
+
         </div>
 
         {/* Description Section */}
